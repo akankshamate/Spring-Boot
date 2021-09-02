9aa5684f8d7e1ac24ceb363dc62cba37518ba4e9
akankshamate <85989430+akankshamate@users.noreply.github.com>
2021-09-02 19:18:44 +0530

    Add files via upload

---
+++ SpringBootCountryServicesUsingHashMap/src/main/java/com/Akanksha/CountryServices/beans/Country.java
@@ -0,0 +1,38 @@
+package com.Akanksha.CountryServices.beans;
+
+public class Country {
+	private int id;
+	private String countryName;
+	private String countryCapital;
+	
+	/**
+	 * @param id
+	 * @param countryName
+	 * @param countryCapital
+	 */
+	public Country(int id, String countryName, String countryCapital) {
+		super();
+		this.id = id;
+		this.countryName = countryName;
+		this.countryCapital = countryCapital;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCountryName() {
+		return countryName;
+	}
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+	public String getCountryCapital() {
+		return countryCapital;
+	}
+	public void setCountryCapital(String countryCapital) {
+		this.countryCapital = countryCapital;
+	}
+	
+}
